e44f519e7dc53bcf1ddd026cfd8d0dbc6117b22c
Vlideing <123292624+Vlideing@users.noreply.github.com>
2023-01-22 16:33:18 +0800

    MakeCode 项目的初始文件

---
+++ main.ts
@@ -0,0 +1,27 @@
+enum RadioMessage {
+    message1 = 49434,
+    Star = 26727
+}
+radio.onReceivedMessage(RadioMessage.Star, function () {
+    mySprite = sprites.create(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . 9 9 9 . . . . . . . . . 
+        . . . . 9 9 9 . . . . . . . . . 
+        . . . . 9 9 9 . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `, SpriteKind.Player)
+})
+let mySprite: Sprite = null
+radio.setGroup(1)
+radio.sendMessage(RadioMessage.Star)
